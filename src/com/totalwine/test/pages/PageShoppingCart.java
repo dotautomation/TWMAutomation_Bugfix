df11917939a4d2973d5468432430098912f9ddd4
Rajat Sud <rajatsud@yahoo.com>
2016-02-12 11:47:41 -0500

    stability

--- src/com/totalwine/test/pages/PageShoppingCart.java
+++ src/com/totalwine/test/pages/PageShoppingCart.java
@@ -5,4 +5,6 @@ import org.openqa.selenium.By;
 public class PageShoppingCart {
 	public static final By CheckOutButton = By.cssSelector("button.chkout-btn");
 	public static final By ZipCodeField = By.id("zipCode");
+	public static final By PromoCodeSubmitButton = By.cssSelector("input.anVoucherForm");
+	public static final By QuantityText = By.cssSelector("input.cart-qty.numonly");
 }
