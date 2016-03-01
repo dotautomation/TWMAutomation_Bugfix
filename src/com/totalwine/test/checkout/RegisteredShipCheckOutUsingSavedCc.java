85f9689d522396436bd1206e11b30b882fde8c08
mhossain <mhossain@.totalwine.com>
2016-03-01 12:23:31 -0500

    Update

--- src/com/totalwine/test/checkout/RegisteredShipCheckOutUsingSavedCc.java
+++ src/com/totalwine/test/checkout/RegisteredShipCheckOutUsingSavedCc.java
@@ -64,7 +64,7 @@ import jxl.read.biff.BiffException;
 							
 							throws InterruptedException, BiffException, IOException {
 
-				logger=report.startTest("Registered Ship Checkout using saved credit card Test");
+				logger=report.startTest("Registered Ship Checkout using saved credit card");
 				driver.get(ConfigurationFunctions.locationSet+Location);
 				Thread.sleep(5000);
 				driver.findElement(By.id("btnYes")).click();
